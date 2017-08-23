f25686a3ee3fa479d76a29990dac75362be9733a
michael <mgebregziabher86@gmail.com>
2017-08-23 13:38:27 -0500

    circle exercise finished with TODO 's' !

--- js/planets-string.js
+++ js/planets-string.js
@@ -5,13 +5,20 @@
     var planetsArray;
 
     // TODO: Convert planetsString to an array, save it to planetsArray.
+    var planetsArray = "mercury, venus, earth, mars, jupiter, saturn, uranus, neptune";
 
     console.log(planetsArray);
 
     // TODO: Create a string with <br> tags between each planet. console.log() your results.
     //       Why might this be useful?
-
+    var resHtml = planetsArray.join("<br>");
+    console.log(resHtml);
     // Bonus: Create a second string that would display your planets in an undordered list.
+    var listHTML = "<ul><li>";
+    listHTML += planetsArray.join("</li><li></li>");
+    listHTML += "</li></ul>";
+    console.log(listHTML);
+    document.body.innerHTML = listHTML;
     //        You will need an opening AND closing <ul> tags around the entire string, and <li> tags around each planet.
     //        console.log() your results.
 })();
